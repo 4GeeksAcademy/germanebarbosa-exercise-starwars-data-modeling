439ce573eb0539e7418e8df4f84502a007502489
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-06-03 02:29:34 +0000

    Se hace el modelado de la base de datos de Star Wars

--- src/models.py
+++ src/models.py
@@ -7,23 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=True)
+    user_name = Column(String(250), nullable=True)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key=True, nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet_name = Column(String(250))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character_name = Column(String(250))
+    user = relationship(User)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+
+    id = Column(Integer, primary_key=True,nullable=True)
+    planet_name = Column(String(250), nullable=True)
+    population = Column(String(250))
+    favorites = relationship(Favorites)
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True,nullable=True)
+    character_name = Column(String(250), nullable=True)
+    age = Column(Integer)
+    favorites = relationship(Favorites)
+
+
 
     def to_dict(self):
         return {}
