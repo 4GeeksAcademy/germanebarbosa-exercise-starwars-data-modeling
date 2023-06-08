280ce3e0b793d8d50abc616db27bebc8e9f9f301
germanebarbosa <119632836+germanebarbosa@users.noreply.github.com>
2023-06-08 03:01:04 +0000

    Se hace la correccion de las relaciones entre las tablas

--- src/models.py
+++ src/models.py
@@ -7,6 +7,21 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
+class Planet(Base):
+    __tablename__ = 'planet'
+
+    id = Column(Integer, primary_key=True, nullable=True)
+    planet_name = Column(String(250), nullable=True)
+    population = Column(String(250))
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True, nullable=True)
+    character_name = Column(String(250), nullable=True)
+    age = Column(Integer)
+
 class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
@@ -14,37 +29,18 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=True)
     user_name = Column(String(250), nullable=True)
     password = Column(String(250))
-    favorites_id = Column(Integer, ForeignKey('favorites.id'))
-    
 
-class Favorites(Base):
+class Favorite(Base):
     __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True, nullable=True)
-    planet_id = Column(Integer, ForeignKey('planet.id'))
-    planet_name = Column(String(250))
-    character_id = Column(Integer, ForeignKey('character.id'))
-    character_name = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
-
-class Planet(Base):
-    __tablename__ = 'planet'
-
-    id = Column(Integer, primary_key=True,nullable=True)
-    planet_name = Column(String(250), nullable=True)
-    population = Column(String(250))
-    favorites = relationship(Favorites)
-
-class Character(Base):
-    __tablename__ = 'character'
-
-    id = Column(Integer, primary_key=True,nullable=True)
-    character_name = Column(String(250), nullable=True)
-    age = Column(Integer)
-    favorites = relationship(Favorites)
-
-
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
 
     def to_dict(self):
         return {}
